e38e6127d060f77cb18798de6103aea378693142
Emily-Lancietti <emily.lancietti@opencontent.it>
2020-02-03 07:53:38 +0100

    Comment search bar and topics links, fix services topics and pagination

--- src/components/Homepage.vue
+++ src/components/Homepage.vue
@@ -6,7 +6,7 @@
                     <nav aria-label="breadcrumb" class="breadcrumb-container">
                         <ol class="breadcrumb">
                             <li class="breadcrumb-item">
-                                <a href="/design-comuni-prototipi/esempi/bootstrap-italia/template-homepage.html">Home</a><span
+                                <router-link :to="{ path: '/' }">Home</router-link><span
                                     class="separator">/</span>
                             </li>
                             <li aria-current="page" class="breadcrumb-item active">
@@ -21,9 +21,8 @@
             <div class="row">
                 <div class="col-lg-7 px-lg-4 py-lg-2">
                     <h1>Servizi</h1>
-                    <p>Donec in consequat nunc. Duis semper fermentum lacus, ac condimentum justo auctor a. Nam erat
-                        erat, porta vel pharetra in, ullamcorper vel turpis.</p>
-                    <div class="form-group mt-5">
+                    <p>Scopri tutti i servizi offerti dal Comune di Trento e <b>fruibili in modalità online</b>, senza bisogno di recarsi agli uffici comunali</p>
+                    <!--div class="form-group mt-5">
                         <form>
                             <input id="ricerca-amministrazione" type="search">
                             <label for="ricerca-amministrazione" style="width: auto;">Cerca contenuti in
@@ -33,8 +32,8 @@
                       xlink:href="/design-comuni-prototipi/assets/bootstrap-italia/dist/svg/sprite.svg#it-search"></use></svg>
             </span>
                         </form>
-                    </div>
-                    <div id="filtri-ricerca-amministrazione">
+                    </div-->
+                    <!--div id="filtri-ricerca-amministrazione">
                         <h6 class="small">Filtri</h6>
                         <div class="chip chip-lg">
                             <span class="chip-label">Tutto</span>
@@ -53,9 +52,9 @@
                                 <span>Aggiungi filtro</span>
                             </button>
                         </div>
-                    </div>
+                    </div-->
                 </div>
-                <div class="col-lg-4 offset-lg-1 pt-5 pt-lg-2">
+                <!--div class="col-lg-4 offset-lg-1 pt-5 pt-lg-2">
                     <div class="link-list-wrapper">
                         <ul class="link-list">
                             <li>
@@ -121,7 +120,7 @@
                             </li>
                         </ul>
                     </div>
-                </div>
+                </div-->
             </div>
         </section>
         <section id="Tutti i servizi">
@@ -135,15 +134,17 @@
                     <div class="row">
                         <div v-for="(service, index) in services" v-bind:key="index" class="col-12 col-sm-6 col-lg-4">
                             <!--start card-->
-                            <article v-if="(index / 6) < currentPage && (index / 6) >= currentPage -1" class="card-wrapper card-space">
+                            <article v-if="(index / 6) < currentPage && (index / 6) >= currentPage -1"
+                                     class="card-wrapper card-space">
                                 <div class="card card-bg card-big rounded shadow">
                                     <div v-if="index === 0" class="flag-icon"></div>
                                     <div v-else class="flag-icon invisible"></div>
-                                    <div class="etichetta">
-                                        <svg class="icon">
-                                            <use xlink:href="bootstrap-italia/dist/svg/sprite.svg#it-pa"></use>
-                                        </svg>
-                                        <span>Scuola e formazione</span>
+                                    <div class="row px-5 d-flex flex-row-reverse">
+                                        <div v-for="(topic, topicindex) in service.topics" v-bind:key="topicindex">
+                                            <div class="chip chip-simple">
+                                                <span class="chip-label">{{ topic.name['ita-IT'] }}</span>
+                                            </div>
+                                        </div>
                                     </div>
                                     <div class="card-body">
                                         <h5 class="card-title">{{service.name}}</h5>
@@ -163,22 +164,25 @@
                     <div class="mt-2">
                         <nav class="pagination-wrapper justify-content-center" aria-label="Esempio di paginazione">
                             <ul class="pagination">
-                                <li class="page-item disabled">
-                                    <a class="page-link" href="#" tabindex="-1" aria-hidden="true">
+                                <li class="page-item">
+                                    <a class="page-link" href="#" tabindex="-1" aria-hidden="true"
+                                       @click="changePage(currentPage -1 , $event)">
                                         <svg class="icon icon-primary">
                                             <use xlink:href="/bootstrap-italia/dist/svg/sprite.svg#it-chevron-left"></use>
                                         </svg>
-                                        <span class="sr-only">Pagina precedente</span>
                                     </a>
                                 </li>
                                 <li v-for="index in this.getPages()" :key="index" class="page-item">
-                                    <a v-if="currentPage === index" class="page-link" href="#" aria-current="page" @click="changePage(index, $event)">
-                                        <span class="d-inline-block d-sm-none">Pagina </span>{{index}}
+                                    <a v-if="currentPage === index" class="page-link" href="#" aria-current="page"
+                                       @click="changePage(index, $event)">
+                                        <span>{{index}}</span>
                                     </a>
-                                <a v-else class="page-link" href="#" @click="changePage(index, $event)">{{index}}</a>
+                                    <a v-else class="page-link" href="#"
+                                       @click="changePage(index, $event)">{{index}}</a>
                                 </li>
-                                <li class="page-item disabled">
-                                    <a class="page-link" href="#" tabindex="-1" aria-hidden="true">
+                                <li class="page-item">
+                                    <a class="page-link" href="#" tabindex="-1" aria-hidden="true"
+                                       @click="changePage(currentPage +1 , $event)">
                                         <span class="sr-only">Pagina successiva</span>
                                         <svg class="icon icon-primary">
                                             <use xlink:href="/bootstrap-italia/dist/svg/sprite.svg#it-chevron-right"></use>
@@ -200,11 +204,12 @@
     data() {
       return {
         services: [],
-        currentPage: 2,
+        currentPage: 1,
       };
     },
     mounted() {
-      this.getServices('https://servizi.comune.trento.it/api/opendata/v2/content/search/classes+%27public_service%27%20sort%20%5Bmodified%3D%3Edesc%5D');
+      this.getServices(
+          'https://servizi.comune.trento.it/api/opendata/v2/content/search/classes+%27public_service%27%20sort%20%5Bmodified%3D%3Edesc%5D');
     },
     methods: {
       getServices(url) {
@@ -215,6 +220,7 @@
               name: item.data['ita-IT'].name,
               abstract: item.data['ita-IT'].abstract,
               link: `https://servizi.comune.trento.it/openpa/object/${item.metadata.id}`,
+              topics: item.data['ita-IT'].topics,
             };
             this.services.push(service);
           });
@@ -228,12 +234,14 @@
         });
       },
       getPages() {
-        return Math.ceil(this.services.length / 6)
+        return Math.ceil(this.services.length / 6);
       },
       changePage(pageNumber, event) {
-        event.preventDefault();
-        this.currentPage = pageNumber;
-      }
+        if (pageNumber > 0 && pageNumber < (this.services.length / 6) + 1) {
+          event.preventDefault();
+          this.currentPage = pageNumber;
+        }
+      },
     },
   };
 </script>
